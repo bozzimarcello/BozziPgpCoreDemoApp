ad2d1f935e03f0538961109a128f6fe2b07b6200
Marcello Bozzi <bozzimarcello@gmail.com>
2022-03-03 19:09:09 +0100

    Generazione, firma e verifica PGP

--- BozziPgpCoreDemoApp/Program.cs
+++ BozziPgpCoreDemoApp/Program.cs
@@ -1,27 +1,69 @@
 ﻿using System;
 using PgpCore;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace BozziPgpCoreDemoApp
 {
     class Program
     {
+        static string basePath = @"E:\_Dev\BozziPgpCoreDemoApp\DemoFiles\";
+
         static void Main(string[] args)
         {
+
             Console.WriteLine("Pgp Core Demo App");
 
-            Verifica();
+            GeneraChiavi();
+
+            Task<string> messaggio;
+
+            messaggio = Firma();
+
+            Console.WriteLine(messaggio.Result);
+
+            messaggio = Verifica();
+
+            Console.WriteLine(messaggio.Result);
 
         }
 
-        public static async void Verifica()
+        public static void GeneraChiavi()
+        {
+            using (PGP pgp = new PGP())
+            {
+                // Generate keys
+                pgp.GenerateKey(basePath + "public.asc", basePath + "private.asc", "email@email.com", "password");
+            }
+            Console.WriteLine("Keys successfully generated");
+        }
+
+        public static async Task<string> Firma()
         {
             // Load keys
-            FileInfo publicKey = new FileInfo(@"C:\TEMP\Keys\public.asc");
+            FileInfo privateKey = new FileInfo(basePath + "private.asc");
+            EncryptionKeys encryptionKeys = new EncryptionKeys(privateKey, "password");
+
+            // Reference input/output files
+            FileInfo inputFile = new FileInfo(basePath + "just-a-file.txt");
+            FileInfo signedFile = new FileInfo(basePath + "just-a-signed-file.pgp");
+
+            // Sign
+            PGP pgp = new PGP(encryptionKeys);
+            await pgp.SignFileAsync(inputFile, signedFile);
+
+            return "File successfully signed";
+
+        }
+
+        public static async Task<string> Verifica()
+        {
+            // Load keys
+            FileInfo publicKey = new FileInfo(basePath + "public.asc");
             EncryptionKeys encryptionKeys = new EncryptionKeys(publicKey);
 
             // Reference input
-            FileInfo inputFile = new FileInfo(@"C:\TEMP\Content\signedContent.pgp");
+            FileInfo inputFile = new FileInfo(basePath + "just-a-signed-file.pgp");
 
             // Verify
             PGP pgp = new PGP(encryptionKeys);
@@ -29,11 +71,11 @@ namespace BozziPgpCoreDemoApp
 
             if (verified)
             {
-                Console.WriteLine("The content is signed with the given public key");
+                return "The content is signed with the given public key";
             }
             else
             {
-                Console.WriteLine("The content is NOT signed with the given public key");
+                return "WARNING The content is NOT signed with the given public key";
             }
         }
     }
