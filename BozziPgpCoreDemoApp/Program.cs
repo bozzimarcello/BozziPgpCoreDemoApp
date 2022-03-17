d9c0510248cca0161563390e8a5797918ce4d96a
Marcello Bozzi <bozzimarcello@gmail.com>
2022-03-17 10:05:27 +0100

    aggiunto chiamata esterna

--- BozziPgpCoreDemoApp/Program.cs
+++ BozziPgpCoreDemoApp/Program.cs
@@ -2,6 +2,7 @@
 using PgpCore;
 using System.IO;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace BozziPgpCoreDemoApp
 {
@@ -13,19 +14,53 @@ namespace BozziPgpCoreDemoApp
         {
 
             Console.WriteLine("Pgp Core Demo App");
+            try
+            {
+                //GeneraChiavi();
 
-            GeneraChiavi();
+                //Task<string> messaggio;
 
-            Task<string> messaggio;
+                //messaggio = Firma();
 
-            messaggio = Firma();
+                //Console.WriteLine(messaggio.Result);
 
-            Console.WriteLine(messaggio.Result);
+                //messaggio = VerificaInChiaro("openSUSE-Leap-15.3-3-NET-x86_64-Build38.1-Media.iso.sha256.asc",
+                //                        "openSUSE-Leap-15.3-3-NET-x86_64-Build38.1-Media.iso.sha256");
+                
+                //Console.WriteLine(messaggio.Result);
 
-            messaggio = Verifica();
+                string messaggio = VerificaChamataEsterna( "openSUSE-Leap-15.3-3-NET-x86_64-Build38.1-Media.iso.sha256.asc",
+                                        "openSUSE-Leap-15.3-3-NET-x86_64-Build38.1-Media.iso.sha256");
 
-            Console.WriteLine(messaggio.Result);
+                //string messaggio = VerificaChamataEsterna("electrum-4.1.5.exe.asc",
+                //                        "electrum-4.1.5.exe");
+                
+                Console.WriteLine(messaggio);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
 
+        public static string VerificaChamataEsterna( string publicKeyFilename, string signedFilename )
+        {
+            string output;
+            using (Process pProcess = new Process())
+            {
+                pProcess.StartInfo.FileName = @"C:\Program Files (x86)\GnuPG\bin\gpg.exe";
+                pProcess.StartInfo.Arguments = "--verify " +
+                    basePath + publicKeyFilename + " " +
+                    basePath + signedFilename; //argument
+                pProcess.StartInfo.UseShellExecute = false;
+                pProcess.StartInfo.RedirectStandardOutput = true;
+                //pProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                //pProcess.StartInfo.CreateNoWindow = true; //not diplay a windows
+                pProcess.Start();
+                output = pProcess.StandardOutput.ReadToEnd(); //The output result
+                pProcess.WaitForExit();
+            }
+            return output;
         }
 
         public static void GeneraChiavi()
@@ -56,18 +91,66 @@ namespace BozziPgpCoreDemoApp
 
         }
 
-        public static async Task<string> Verifica()
+        public static async Task<string> Verifica(string publicKeyFilename, string signedFilename)
         {
             // Load keys
-            FileInfo publicKey = new FileInfo(basePath + "public.asc");
+            FileInfo publicKey = new FileInfo(basePath + publicKeyFilename);
             EncryptionKeys encryptionKeys = new EncryptionKeys(publicKey);
 
             // Reference input
-            FileInfo inputFile = new FileInfo(basePath + "just-a-signed-file.pgp");
+            FileInfo inputFile = new FileInfo(basePath + signedFilename);
 
             // Verify
             PGP pgp = new PGP(encryptionKeys);
             bool verified = await pgp.VerifyFileAsync(inputFile);
+
+            if (verified)
+            {
+                return "The content is signed with the given public key";
+            }
+            else
+            {
+                return "WARNING The content is NOT signed with the given public key";
+            }
+        }
+
+        public static async Task<string> VerificaStream(string publicKeyFilename, string signatureFilename, string downloadedFilename)
+        {
+            // Load keys
+            EncryptionKeys encryptionKeys;
+            using (Stream publicKeyStream = new FileStream(basePath + publicKeyFilename, FileMode.Open))
+                encryptionKeys = new EncryptionKeys(publicKeyStream);
+
+            PGP pgp = new PGP(encryptionKeys);
+
+            // Reference input file
+            bool verified;
+            using (FileStream inputFileStream = new FileStream(basePath + signatureFilename, FileMode.Open))
+                // Verify
+                verified = await pgp.VerifyStreamAsync(inputFileStream);
+
+            if (verified)
+            {
+                return "The content is signed with the given public key";
+            }
+            else
+            {
+                return "WARNING The content is NOT signed with the given public key";
+            }
+        }
+
+        public static async Task<string> VerificaInChiaro(string publicKeyFilename, string signedFilename)
+        {
+            // Load keys
+            FileInfo publicKey = new FileInfo(basePath + publicKeyFilename);
+            EncryptionKeys encryptionKeys = new EncryptionKeys(publicKey);
+
+            // Reference input
+            FileInfo inputFile = new FileInfo(basePath + signedFilename);
+
+            // Verify
+            PGP pgp = new PGP(encryptionKeys);
+            bool verified = await pgp.VerifyClearFileAsync(inputFile);
 
             if (verified)
             {
